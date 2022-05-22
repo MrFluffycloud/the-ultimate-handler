2fa2b298ebcfea0caa2237e3008f81f745ae401b
MrFluffycloud <aliadraseen@gmail.com>
2022-05-22 14:34:44 +0530

    v0.0.9 - Final Release Soon

---
+++ docusaurus.config.js
@@ -0,0 +1,127 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/vsLight');
+const darkCodeTheme = require('prism-react-renderer/themes/vsDark');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'U-DJS Docs',
+  tagline: 'The Ultimate Handler',
+  url: 'https://udjs.mrfluffycloud.xyz.com',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'MrFluffycloud', // Usually your GitHub org/user name.
+  projectName: 'the-ultimate-handler', // Usually your repo name.
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/MrFluffycloud/the-ultimate-handler/tree/wiki',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/MrFluffycloud/the-ultimate-handler/tree/wiki',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'U - DJS',
+        logo: {
+          alt: 'UDJS',
+          src: 'img/logo.png',
+        },
+        items: [
+          {
+            type: 'doc',
+            docId: 'intro',
+            position: 'left',
+            label: 'Tutorial',
+          },
+          {to: '/blog', label: 'Updates', position: 'left'},
+          {
+            href: 'https://github.com/MrFluffycloud/the-ultimate-handler',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'Tutorial',
+                to: '/docs/intro',
+              },
+            ],
+          },
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'Discord',
+                href: 'https://discordapp.com/invite/9Eg7Ngj2PQ',
+              },
+              {
+                label: 'Twitter',
+                href: 'https://twitter.com/MrFluffycloud',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'Blog',
+                to: '/blog',
+              },
+              {
+                label: 'GitHub',
+                href: 'https://github.com/MrFluffycloud/the-ultimate-handler',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} Ultimate DJS Handler, Inc. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
---
+++ src/pages/index.md
@@ -0,0 +1,69 @@
+---
+title: Home
+---
+
+<div align="center">
+  <br />
+  <p>
+    <img src="https://media.discordapp.net/attachments/714722763000250428/977589843246854234/udjs.png?width=1440&height=260" alt="TheHandler" />
+  </p>
+  <br />
+  <p>
+    <a href="https://discord.com/invite/9Eg7Ngj2PQ"><img alt="Discord" src="https://img.shields.io/discord/842777291821285417?color=5865F2&label=chat&logo=discord&logoColor=white"/></a>
+    <a href="#"><img alt="Version)" src="https://img.shields.io/github/package-json/v/MrFluffycloud/the-ultimate-handler/main"/></a>
+    <a href="#"><img alt="GitHub Repo stars" src="https://img.shields.io/github/stars/MrFluffycloud/the-ultimate-handler?label=Stars&logo=github&logoColor=white?style=plastic"/></a>
+    <a href="https://github.com/prettier/prettier"><img src="https://img.shields.io/badge/code_style-prettier-ff69b4.svg?style=flat-square" alt="Discord server" /></a>
+    
+  </p>
+</div>
+
+## About
+
+A Highly Customizable Discord Handler,
+Made for [Discord.js v13](https://discord.js.org)<br/>
+Made with 🤍 By [MrFluffycloud](https://mrfluffycloud.xyz)<br/>
+
+- Slash Commands
+- Auto Loads, Both Globbaly and Private
+- Stable & Tested
+- Lots of Features
+
+> This Project is worked on daily, and is updated constantly, Join our discord and spot us some ideas - [Discord Server](https://discord.gg/9Eg7Ngj2PQ)!
+
+## Installation
+
+**Node.js 16.9.0 or newer is required.**
+
+```cli
+git clone https://github.com/MrFluffycloud/the-ultimate-handler.git
+cd the-ultimate-handler
+npm install
+npm run setup
+```
+
+### Plugins
+
+- Coming Soon..
+
+## Guide
+
+> Setup Guide - [Start](/docs/intro)
+
+## Links
+
+- [Handler Guide](https://udjs.mrfluffycloud.xyz)
+- [Discord.js Website](https://discord.js.org)
+- [Discord.js Guide](https://discordjs.guide)
+- [discord.js Discord server](https://discord.gg/djs)
+- [GitHub](https://github.com/discordjs/discord.js)
+
+## Contributing
+
+Want to help us improve this project? You are more than welcome to do so!
+Let us know if you find some issues thru here - [Here](https://github.com/MrFluffycloud/the-ultimate-handler/issues)<br/>
+
+## Help
+
+- Handler Related Questions - [Support Server](https://discord.gg/9Eg7Ngj2PQ).
+- Discord.js Related Questions - [discord.js Server](https://discord.gg/djs).
+- Coding in General Related Questions - [Programer Hangout Discord Server](https://discord.gg/programming)
